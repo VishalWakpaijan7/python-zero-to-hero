b97a51f47cb9dc87655e59456defdd63001a1b32
Vishal W <wakpaijanvishal@gmail.com>
2024-08-23 00:03:03 +0530

    basic python done

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,22 @@
 print('Hello, World!')
+
+# Checking editor python version
+import sys
+print(sys.version)
+
+# Python indentation
+if 5 > 2:
+    print('Five is greater then two!')
+
+# Comments in python
+
+# used to declare single line comment
+
+""" 
+
+Triple quotes are used to declare multi line commit as python don't have any other way
+String declared in triple quotes don't consider as string as it's does not belongs to any variable
+
+"""
+
+# 

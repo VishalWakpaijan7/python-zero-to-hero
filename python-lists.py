84ad2e0cdab8902960dfca093cb2b128e908a332
Vishal W <wakpaijanvishal@gmail.com>
2024-09-12 11:10:19 +0530

    change in list item done

--- python-lists.py
+++ python-lists.py
@@ -69,3 +69,27 @@ fruitList = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
 
 # if 'apple' in fruitList:
 #     print("Yes, 'apple' is in the fruits list")
+
+# * ################## Change in list items ############
+
+# * Change item value : To change the value of a specific item, refer to the index number
+
+# fruitList[1] = "dragon-fruit"
+# print(fruitList)
+
+# * Change a Range of Item Values
+# ? To change the value of items within a specific range, define a list with the new values, and refer to the range of index numbers where you want to insert the new values
+
+# fruitList[1:3] = ['dragon-fruit','pear','watermelon']
+# print(fruitList, len(fruitList))
+
+# * If you insert less items than you replace, the new items will be inserted where you specified, and the remaining items will move accordingly
+# * Note: The length of the list will change when the number of items inserted does not match the number of items replaced.
+
+# * Insert Items 
+"""
+ * To insert a new list item, without replacing any of the existing values, we can use the insert() method.
+ * The insert() method inserts an item at the specified index
+"""
+# fruitList.insert(2,'watermelon')
+# print(fruitList)

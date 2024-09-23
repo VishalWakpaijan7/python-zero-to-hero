7019f4616e9c48a93e2e37c1f6f5e6481ff9e780
Vishal W <wakpaijanvishal@gmail.com>
2024-09-23 11:01:49 +0530

    python maths

---
+++ python-maths.py
@@ -0,0 +1,47 @@
+"""
+PYTHON MATHS
+Python has a set of built-in math functions, including an extensive math module, that allows you to perform mathematical tasks on numbers.
+"""
+
+# * Built-in maths functions
+
+# x = min(5,3,8)
+# y = max(5,3,8)
+
+# print(x,y)
+
+# * The abs() function returns the absolute (positive) value of the specified number:
+
+# x = abs(-7.25)
+# print(x)
+
+#* The pow(x, y) function returns the value of x to the power of y (xy).
+
+# x = pow(2,3)
+# print(x)
+
+# * The math module:
+"""
+Python has also a built-in module called math, which extends the list of mathematical functions.
+To use it, you must import the math module:
+"""
+
+# import math
+
+# * math.sqrt() : The math.sqrt() method for example, returns the square root of a number:
+
+# print(math.sqrt(64))
+
+# * math.ceil() and math.floor() :
+"""
+The math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer, and returns the result:
+"""
+
+# x = math.ceil(1.4)
+# y = math.floor(1.4)
+
+# print(x,y)
+
+# * math.pie() : The math.pi constant, returns the value of PI (3.14...):
+
+# print(math.pi) # * 3.141592653589793
